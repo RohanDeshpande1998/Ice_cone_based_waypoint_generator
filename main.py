e1071c7619dca6f6b7017de8c9166513c95e04d2
RohanAD <rohandeshpande55@gmail.com>
2024-12-14 14:38:45 +0530

    Bug fixes

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import math
 import numpy as np
 
 def generate_waypoint(robot_motion_vector, goal, sensor_data_points):
-    """_summary_
+##TODO: instead of using robot position we should use robot heading for robot_motion_vector
+
+    """This function is built with the inspiration of Vijay Karthik's paper: 'Choreographing Safety: Planning via Ice-cone-Inspired Motion Sets of
+Feedback Controllers for Car-like Robots'. He has defined this algorithm in 2 dimensions, this is built for 3 dimensions. 
 
     Args:
         robot_motion_vector (vector3d): vector defining where the robot is going to move
@@ -12,8 +15,12 @@ def generate_waypoint(robot_motion_vector, goal, sensor_data_points):
 
     Returns:
         waypoint (vector3d): vector from robot frame of reference
-        cone_axis_angle (float): cone angle in radians
+        cone_axis (vector3d): cone axis w.r.t robot
+        cone_slant_length (float): slant length of cone
         sphere_radius (float): radius of sphere that touches the cone
+        
+
+    Dev: RohanD
     """
     robot_motion_vector_hat = robot_motion_vector/np.linalg.norm(robot_motion_vector)
     robot_to_sphere_center_distance_candidates = []
@@ -26,7 +33,9 @@ def generate_waypoint(robot_motion_vector, goal, sensor_data_points):
         dot_products = np.clip(np.dot(sensor_data_point_directions, direction), -1.0, 1.0)
         valid_mask_dot_products_non_negative = dot_products >= 0
         valid_mask_dot_product_less_than_axis_angle = dot_products >= np.dot(direction,robot_motion_vector_hat)
+
         valid_points_mask = valid_mask_dot_products_non_negative & valid_mask_dot_product_less_than_axis_angle
+
         if np.any(valid_points_mask):
             valid_sensor_data_point_directions = sensor_data_point_directions[valid_points_mask]
             valid_sensor_data_point_distances = sensor_data_point_distances[valid_points_mask]
@@ -42,10 +51,13 @@ def generate_waypoint(robot_motion_vector, goal, sensor_data_points):
     cone_axis_candidates = np.array(cone_axis_candidates)
     cone_axis_angle_candidates = np.array(cone_axis_angle_candidates)
     robot_to_sphere_center_distance_candidates = np.array(robot_to_sphere_center_distance_candidates)
-    d = 0
 
     waypoint_candidates = cone_axis_candidates*robot_to_sphere_center_distance_candidates[:, np.newaxis]
+
     min_dist = math.inf
+    cone_axis_angle = 0 
+    waypoint = []
+    d = 0
     for index, possible_waypoint in enumerate(waypoint_candidates):
         curr_dist = np.linalg.norm(possible_waypoint - goal)
         if curr_dist < min_dist:
@@ -55,8 +67,8 @@ def generate_waypoint(robot_motion_vector, goal, sensor_data_points):
             d = robot_to_sphere_center_distance_candidates[index]
             cone_axis = cone_axis_candidates[index]
     sphere_radius = d * math.sin(cone_axis_angle)
-
-    return waypoint, cone_axis_angle, sphere_radius 
+    cone_slant_length = d * math.cos(cone_axis_angle)
+    return waypoint, cone_axis, cone_slant_length, sphere_radius
     
 
 
@@ -74,7 +86,7 @@ def main():
     goal = np.array([1, 1, 1])
     robot_motion_vector = np.array([-1,3,0])
 
-    generate_waypoint(robot_motion_vector,goal,points)
-
+    waypoint, cone_axis, slant_length, sphere_radius = generate_waypoint(robot_motion_vector,goal,points)
+    print(f"waypoint:{waypoint}, cone_axis:{cone_axis}, slant_length: {slant_length}, sphere_radius: {sphere_radius}")
 if __name__ == "__main__":
     main()
